99ee6a7e71e89a44a2006d82e865f1fd26a41e15
Odailson Silva <odafreelancerti@gmail.com>
2021-08-10 09:23:28 -0300

    fix2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,11 +11,6 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Eduick</title>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -1,30 +1,54 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+  // static async getInitialProps(ctx: DocumentContext) {
+  //   const sheet = new ServerStyleSheet()
+  //   const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        })
+  //   try {
+  //     ctx.renderPage = () =>
+  //       originalRenderPage({
+  //         enhanceApp: (App) => (props) =>
+  //           sheet.collectStyles(<App {...props} />)
+  //       })
 
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      }
-    } finally {
-      sheet.seal()
-    }
+  //     const initialProps = await Document.getInitialProps(ctx)
+  //     return {
+  //       ...initialProps,
+  //       styles: (
+  //         <>
+  //           {initialProps.styles}
+  //           {sheet.getStyleElement()}
+  //         </>
+  //       )
+  //     }
+  //   } finally {
+  //     sheet.seal()
+  //   }
+  // }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
