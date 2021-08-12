5248ea508c58d30a98ffbbedcd62dcb16697135d
Odailson Silva <odafreelancerti@gmail.com>
2021-08-12 09:43:07 -0300

    feat: Button - Documentacao e Testes

--- src/components/atoms/Button/stories.tsx
+++ src/components/atoms/Button/stories.tsx
@@ -1,9 +1,68 @@
+/* eslint-disable react/display-name */
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { Button } from '.'
+import { addParameters } from '@storybook/react'
+import { DocsPage, DocsContainer } from '@storybook/addon-docs'
+import {
+  Title,
+  Description,
+  ArgsTable,
+  PRIMARY_STORY,
+  Stories,
+  Subtitle,
+  Primary
+} from '@storybook/addon-docs'
+import { Button, ButtonProps } from '.'
+
+addParameters({
+  docs: {
+    container: DocsContainer,
+    page: DocsPage
+  }
+})
 
 export default {
   title: 'Button',
-  component: Button
+  component: Button,
+  argTypes: {
+    themeButton: {
+      description: 'Theme a ser aplicado',
+      control: {
+        type: 'select'
+      }
+    }
+  },
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title>Button</Title>
+          <Subtitle />
+          <Description>
+            Componente usado para controlar ações do usuário.
+          </Description>
+          <Primary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      )
+    }
+  }
 } as Meta
 
-export const Basic: Story = () => <Button>Button</Button>
+export const Basic: Story<ButtonProps> = (args) => (
+  <Button {...args}>Button</Button>
+)
+
+export const Secondary: Story<ButtonProps> = (args) => (
+  <Button {...args}>Button</Button>
+)
+Secondary.args = {
+  themeButton: 'secondary'
+}
+
+export const Danger: Story<ButtonProps> = (args) => (
+  <Button {...args}>Button</Button>
+)
+Danger.args = {
+  themeButton: 'danger'
+}
--- src/components/atoms/Button/test.tsx
+++ src/components/atoms/Button/test.tsx
@@ -1,11 +1,60 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from '../../../utils/tests/helpers'
+import theme from '../../../styles/theme'
 
 import { Button } from '.'
+import { transparentize } from 'polished'
 
 describe('<Button />', () => {
-  it('shold render the heading', () => {
-    // const { container } = render(<Button />)
-    // expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
-    // expect(container.firstChild).toMatchSnapshot()
-  })
+  it('shold render button default', () => {
+    renderWithTheme(<Button>Button</Button>)
+    const button = screen.getByRole('button', { name: /Button/i })
+
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveStyle({
+      background: theme.colors.yellow[500],
+      color: '#6f48ec'
+    })
+  }),
+    it('shold render button theme secondary', () => {
+      renderWithTheme(<Button themeButton="secondary">Button</Button>)
+      const button = screen.getByRole('button', { name: /Button/i })
+
+      expect(button).toBeInTheDocument()
+      expect(button).toHaveStyle({
+        background: transparentize(0.7, theme.colors.purple[900]),
+        color: theme.colors.white[500]
+      })
+    }),
+    it('shold render button theme danger', () => {
+      renderWithTheme(<Button themeButton="danger">Button</Button>)
+      const button = screen.getByRole('button', { name: /Button/i })
+
+      expect(button).toBeInTheDocument()
+      expect(button).toHaveStyle({
+        background: '#f44336',
+        color: theme.colors.white[500]
+      })
+    }),
+    it('shold render button theme danger', () => {
+      renderWithTheme(<Button disabled>Button</Button>)
+      const button = screen.getByRole('button', { name: /Button/i })
+
+      expect(button).toBeInTheDocument()
+      expect(button).toHaveStyle({ cursor: 'not-allowed' })
+    }),
+    it('shold render button clicks', () => {
+      const onClick = jest.fn()
+
+      const { container } = renderWithTheme(
+        <Button onClick={onClick}>Button</Button>
+      )
+      const button = screen.getByRole('button', { name: /Button/i })
+
+      fireEvent.click(button)
+
+      expect(onClick).toHaveBeenCalled()
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
 })
